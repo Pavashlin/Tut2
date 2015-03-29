d3c95e4e0330f799f932ab06ddca75ab8a07e60f
Pavashlin <pavashlingovender@yahoo.com>
2015-03-29 16:08:59 +0200

    added test, link bugs appear

--- FractionTest/unittest1.cpp
+++ FractionTest/unittest1.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "Fraction.h"
+#include <iostream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+using namespace std;
 namespace FractionTest
+	
 {		
 	TEST_CLASS(UnitTest1)
 	{
@@ -12,6 +14,10 @@ namespace FractionTest
 		
 		TEST_METHOD(TestMethod1)
 		{
+			Fraction a, b;
+			a.setn(1);
+			a.setd(0);
+			Assert::AreNotEqual(0, a.getd(),L"Denominator is zero",LINE_INFO());
 			// TODO: Your test code here
 		}
 
--- Tut2/Fraction.h
+++ Tut2/Fraction.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma once
 #ifndef FRACTION_H
 #define FRACTION_H
 using namespace std;
